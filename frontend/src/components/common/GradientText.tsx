1d9179a81947379e2df02eccb48275260b040d9e
tilak-dev <tsingh99991@gmail.com>
2025-04-15 17:16:55 +0530

    Enhance HomePage layout with GradientText component and add description; create LinkShortner component

--- frontend/src/components/common/GradientText.tsx
+++ frontend/src/components/common/GradientText.tsx
@@ -10,7 +10,7 @@ export default function GradientText({ children, className }: Props) {
   return (
     <div
       className={cn(
-        "text-sm font-bold cursor-pointer bg-gradient-to-r from-pink-600 via-indigo-500 to-blue-500 bg-clip-text text-transparent hover:via-70% ",
+        "text-sm font-bold cursor-pointer bg-gradient-to-r from-pink-600 via-indigo-500 to-blue-500 bg-clip-text text-transparent hover:via-70% pb-2 ",
         className
       )}
     >
--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,10 +1,25 @@
 import Container from "@/components/common/Container";
+import GradientText from "@/components/common/GradientText";
 
 export default function HomePage() {
   return (
     <>
       <Container>
-        hii
+        {/* hero section */}
+        <section
+          id="home"
+          className="flex flex-col items-center justify-center "
+        >
+          <GradientText className="md:text-5xl text-3xl font-bold text-center tracking-wide max-w-4xl leading-snug">
+            Shorten your links and track your clicks with Linkify :)
+          </GradientText>
+          <p className="text-gray-500 text-[16.5px] font-normal text-center max-w-2xl ">
+            Linkify is a URL shortening service that allows you to create short
+            links for your long URLs. You can also track the number of clicks on
+            your short links.
+          </p>
+          {/* link shortner  */}
+        </section>
       </Container>
     </>
   );

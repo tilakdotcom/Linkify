179a6110a7c83829f4ef6a4cb4819ffc532e4a45
tilak-dev <tsingh99991@gmail.com>
2025-04-16 16:19:01 +0530

    feat: add LoginPage component with form handling and validation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import MainLayout from "./layout/MainLayout";
 import SignupPage from "./pages/auth/SignupPage";
+import LoginPage from "./pages/auth/LoginPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<MainLayout />}>
           <Route index element={<HomePage />} />
           <Route path="/register" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/user" element={<div>user</div>} />
         </Route>
       </Routes>

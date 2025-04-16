97e05bbf7218ca7e8ec409df2a3ea6bd5eb17ccb
tilak-dev <tsingh99991@gmail.com>
2025-04-16 13:58:55 +0530

    backend init

---
+++ backend/src/core/controllers/user.controller.ts
@@ -0,0 +1,60 @@
+import appAssert from "../../common/API/AppAssert";
+import { emailSchema } from "../../common/schemas/auth";
+import { passwordChangeSchema } from "../../common/schemas/user";
+import { BAD_REQUEST, OK } from "../../common/constants/http";
+import asyncHandler from "../../middlewares/asyncHandler.middleware";
+import { validateFileImage } from "../../middlewares/file.middleware";
+import {
+  userAvatarService,
+  userPasswordChangeService,
+  userPasswordResetRequestService
+} from "../services/user.service";
+
+export const userAccessHandler = asyncHandler(async (req, res) => {
+  return res.status(OK).json({
+    message: "User authenticated successfully",
+  });
+});
+
+export const userProfileImageHandler = asyncHandler(async (req, res) => {
+  const userId = req.userId;
+  appAssert(req.file, BAD_REQUEST, "avatar not found");
+  const { path } = validateFileImage(req.file as Express.Multer.File);
+
+  const { user } = await userAvatarService({
+    avatar: path,
+    userId: userId as string,
+  });
+
+  return res.status(OK).json({
+    message: "Avatar successfully uploaded ",
+    user,
+  });
+});
+
+export const userResetPasswordHandler = asyncHandler(async (req, res) => {
+  const email = emailSchema.parse(req.body.email);
+
+  await userPasswordResetRequestService({ email });
+
+  return res.status(OK).json({
+    message: "Password reset email sent successfully ",
+  });
+});
+
+export const userPasswordChangeHandler = asyncHandler(async (req, res) => {
+  const body = passwordChangeSchema.parse({
+    ...req.body,
+    token: req.params.token,
+  });
+
+  const { user } = await userPasswordChangeService({
+    newPassword: body.newPassword,
+    passwordResetToken: body.token,
+  });
+
+  return res.status(OK).json({
+    message: "Password reset successfully",
+    data: user,
+  });
+});
---
+++ backend/src/core/services/auth.service.ts
@@ -0,0 +1,139 @@
+import appAssert from "../../common/API/AppAssert";
+import { passwordCompare, passwordHasher } from "../../common/utils/bcryptjs";
+import { Now, thirtyDaysFromNow } from "../../common/utils/customTime";
+import {
+  accessTokenSignOptions,
+  generateToken,
+  refreshTokenSignOptions,
+  verifyToken,
+} from "../../common/utils/jwtHelper";
+import { BAD_REQUEST, UNAUTHORIZED } from "../../common/constants/http";
+import prisma from "../../database/dbConnect";
+
+
+type CreateUserData = {
+  email: string;
+  password: string;
+  username: string;
+};
+
+export const createUserService = async (data: CreateUserData) => {
+  const userExists = await prisma.user.findFirst({
+    where: { email: data.email },
+  })
+
+  appAssert(!userExists, BAD_REQUEST, "user already exists");
+
+  const hashedPassword = await passwordHasher(data.password)
+
+  const user = await prisma.user.create({
+    data: {
+      username: data.username,
+      email: data.email,
+      password: hashedPassword,
+    }
+  })
+
+  const {password, ...rest} = user
+
+  return {
+    user:rest
+  };
+};
+
+type LoginUserData = {
+  userAgent?: string;
+  email: string;
+  password: string;
+};
+
+export const loginUserService = async (data: LoginUserData) => {
+  const user = await prisma.user.findFirst({
+    where: { email: data.email },
+  });
+
+  //validation
+  appAssert(user, BAD_REQUEST, "invalid login user details");
+
+  //password check
+  const isMatch = await passwordCompare(data.password, user.password)
+  appAssert(isMatch, BAD_REQUEST, "invalid login user or password details");
+
+  //create session
+  const session = await prisma.session.create({
+   data: {
+     userId: user.id,
+     userAgent: data.userAgent,
+     expiresAt: thirtyDaysFromNow(),
+   },
+  });
+
+  //generate tokens
+
+  const refreshToken = generateToken(
+    {
+      userId: user.id,
+      sessionId: session.id,
+    },
+    refreshTokenSignOptions
+  );
+
+  const accessToken = generateToken(
+    {
+      userId: user.id,
+      sessionId: session.id,
+    },
+    accessTokenSignOptions
+  );
+  const updateSession = await prisma.session.update({
+    where: { id: session.id },
+    data: { refreshToken },
+  })
+
+  const {password, ...rest} = user
+
+  return {
+    user: rest,
+    accessToken,
+    refreshToken,
+    updateSession,
+  };
+};
+
+export const refreshTokenService = async (refreshToken: string) => {
+  const userId = verifyToken({
+    token: refreshToken,
+    options: refreshTokenSignOptions,
+  });
+
+  appAssert(userId.userId, UNAUTHORIZED, "invalid  refresh token");
+
+  const session = await prisma.session.findFirst({
+    where: {
+      id: userId.sessionId,
+      refreshToken: refreshToken,
+      expiresAt: {
+        gte: Now()
+      },
+    },
+  })
+
+  appAssert(
+    session && session.refreshToken === refreshToken,
+    UNAUTHORIZED,
+    "session not found  in the database or refresh token is invalid"
+  );
+
+  const accessToken = generateToken(
+    {
+      userId: session.userId,
+      sessionId: session.id,
+    },
+    accessTokenSignOptions
+  );
+
+  return {
+    accessToken,
+    session,
+  };
+};

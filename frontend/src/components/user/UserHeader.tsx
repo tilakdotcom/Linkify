f32d7b8fd7a44de1d1f89b3ec60e32bb785dd046
tilak-dev <tsingh99991@gmail.com>
2025-04-20 16:28:17 +0530

    UserHeader to accept active state and setActive function; integrate with UserPage

--- frontend/src/components/user/UserHeader.tsx
+++ frontend/src/components/user/UserHeader.tsx
@@ -3,9 +3,15 @@ import { cn } from "@/common/lib/utils";
 
 type UserHeaderProps = {
   className?: string;
+  active: string;
+  setActive: (name: string) => void;
 };
 
-export default function UserHeader({ className }: UserHeaderProps) {
+export default function UserHeader({
+  className,
+  active,
+  setActive,
+}: UserHeaderProps) {
   return (
     <div className={cn(" flex  justify-center bg-gray-800 py-2", className)}>
       <div className="w-full flex gap-x-1.5 items-center justify-between max-w-2xl">
@@ -14,7 +20,12 @@ export default function UserHeader({ className }: UserHeaderProps) {
           return (
             <button
               key={i}
-              className="flex gap-x-1.5 justify-center items-center focus:outline-none cursor-pointer border border-b-2 border-x-0 border-t-0 border-transparent hover:border-blue-500 pb-1 transition-colors  "
+              onClick={() => setActive(icons.name)}
+              className={`flex gap-x-1.5 justify-center items-center focus:outline-none cursor-pointer border border-b-[3px] border-x-0 border-t-0 hover:border-blue-500 pb-1 transition-colors text-sm ${
+                active === icons.name
+                  ? "border-blue-500"
+                  : " border-transparent "
+              }`}
             >
               <Icon className="size-4 font-medium" />{" "}
               <span className="capitalize font-mediummd">{icons.name}</span>
--- frontend/src/pages/user/UserPage.tsx
+++ frontend/src/pages/user/UserPage.tsx
@@ -1,11 +1,13 @@
 import Container from "@/components/common/Container";
 import UserHeader from "@/components/user/UserHeader";
+import { useState } from "react";
 
 export default function UserPage() {
+  const [active, setActive] = useState<string>("history");
   return (
     <Container className="py-0 min-h-screen">
       {/* "user header" */}
-      <UserHeader />
+      <UserHeader active={active} setActive={setActive} />
     </Container>
   );
 }

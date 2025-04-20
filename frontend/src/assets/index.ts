fbcb88c7ef651c1aa934085b43216b986fd48c2f
tilak-dev <tsingh99991@gmail.com>
2025-04-20 17:09:45 +0530

    feat: add UnderDevelopment component for indicating in-progress sections

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,9 +1,5 @@
 import rootBGPng from "./rootBG.png";
-import signupImage from "./signupImage.png"
+import signupImage from "./signupImage.png";
+import development from "./development.svg";
 
-
-
-
-
-
-export { rootBGPng , signupImage };
+export { rootBGPng, signupImage, development };
--- frontend/src/pages/user/UserPage.tsx
+++ frontend/src/pages/user/UserPage.tsx
@@ -1,4 +1,5 @@
 import Container from "@/components/common/Container";
+import UnderDevelopment from "@/components/common/UnderDevelopment";
 import UserHeader from "@/components/user/UserHeader";
 import UserHistory from "@/components/user/UserHistory";
 import { useState } from "react";
@@ -9,7 +10,9 @@ export default function UserPage() {
     <Container className="py-0 min-h-screen">
       {/* "user header" */}
       <UserHeader active={active} setActive={setActive} />
-      {active ==="history" && <UserHistory />}
+      {active === "history" && <UserHistory />}
+      {active === "statistics" && <UnderDevelopment />}
+      {active === "clicks" && <UnderDevelopment />}
     </Container>
   );
 }

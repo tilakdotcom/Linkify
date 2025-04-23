c8a412e33864661e29f3e65bdc86a2ff92a9a7a0
tilak-dev <tsingh99991@gmail.com>
2025-04-23 16:55:54 +0530

    design done

--- frontend/src/components/app-ui/LinkShortner.tsx
+++ frontend/src/components/app-ui/LinkShortner.tsx
@@ -8,7 +8,7 @@ import { z } from "zod";
 import toast from "react-hot-toast";
 import { FormProvider } from "react-hook-form";
 import { Input } from "../ui/input";
-import { FormField, FormItem, FormControl, FormMessage } from "../ui/form";
+import { FormField, FormItem, FormControl } from "../ui/form";
 
 export default function LinkShortner() {
   const form = useForm<z.infer<typeof uriSchema>>({
@@ -18,42 +18,63 @@ export default function LinkShortner() {
     },
   });
 
+  const {
+    formState: { errors },
+  } = form;
+
   function onSubmit(data: z.infer<typeof uriSchema>) {
     toast.success("Shortening the URL...");
     console.log("Submitted URL:", data.longUrl);
   }
 
   return (
-    <CommonDiv className="mt-2 space-y-4">
-      <FormProvider {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="flex items-center">
-          <FormField
-            control={form.control}
-            name="longUrl"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <div className="flex items-center justify-center pl-3">
-                    <GoLink className="md:size-5 text-white/80" />
-                    <Input
-                      className="bg-transparent placeholder:text-sm text-sm text-white/80 focus:outline-none pl-2 py-1 rounded-full rounded-r-none md:w-sm w-full border-none focus:ring-0 focus:border-none  focus-visible:ring-0 "
-                      placeholder="Enter the link here"
-                      {...field}
-                    />
-                  </div>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+    <div className="flex flex-col items-center justify-center space-y-1">
+      <CommonDiv className="mt-2 ">
+        <FormProvider {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex items-center"
+          >
+            <FormField
+              control={form.control}
+              name="longUrl"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="flex items-center justify-center pl-3">
+                      <GoLink className="md:size-5 text-white/80" />
+                      <Input
+                        autoComplete="off"
+                        className={`bg-transparent hover:bg-transparent placeholder:text-sm text-sm text-white/80 focus:outline-none pl-2 py-1 rounded-full rounded-r-none md:w-sm w-full border-none focus:ring-0 focus:border-none  focus-visible:ring-0 `}
+                        placeholder="Enter the link here"
+                        {...field}
+                      />
+                    </div>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <div>
+              <CustomButtonBlue
+                disabled={errors.longUrl?.message ? true : false}
+                type="submit"
+                className={`md:text-[14px] py-1.5 ${
+                  errors.longUrl?.message
+                    ? "cursor-not-allowed bg-blue-500 hover:bg-blue-500"
+                    : ""
+                } `}
+              >
+                Shorten Now
+              </CustomButtonBlue>
+            </div>
+          </form>
+        </FormProvider>
+      </CommonDiv>
 
-          <div>
-            <CustomButtonBlue type="submit" className="md:text-[14px] py-1.5">
-              Shorten Now
-            </CustomButtonBlue>
-          </div>
-        </form>
-      </FormProvider>
-    </CommonDiv>
+      <div className="text-red-500 text-xs">
+        {errors.longUrl?.message && errors.longUrl.message}
+        {!errors.longUrl?.message && <span>&nbsp;</span>}
+      </div>
+    </div>
   );
 }

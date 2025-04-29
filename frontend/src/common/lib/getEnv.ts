3147aafdaf441507dfd0501e1eee0238f9f48d7c
tilak-dev <tsingh99991@gmail.com>
2025-04-29 16:25:58 +0530

    OAuth 2.0  init

--- frontend/src/common/lib/getEnv.ts
+++ frontend/src/common/lib/getEnv.ts
@@ -8,3 +8,11 @@ export const getEnv = (env: string, defaultValue?: string) => {
 
 export const backendUri = getEnv("VITE_BACKEND_URI");
 export const frontendUri = getEnv("VITE_FRONTEND_URI");
+export const CLIENT_ID = getEnv("CLIENT_ID") 
+export const PROJECT_ID = getEnv("PROJECT_ID")
+export const AUTH_URI = getEnv("AUTH_URI")
+export const TOKEN_URI = getEnv("TOKEN_URI")
+export const AUTH_PROVIDER = getEnv("AUTH_PROVIDER")
+export const CLIENT_SECRET = getEnv("CLIENT_SECRET")
+export const REDIRECT_URI = getEnv("REDIRECT_URI")
+export const JAVASCRIPT_URI = getEnv("JAVASCRIPT_URI")
